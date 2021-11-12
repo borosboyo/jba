af58ad9d3d36983ace77135e452f2f4d677200f1
borospc <borosgergo@yahoo.com>
2021-11-12 12:35:16 +0100

    mongodb done

---
+++ middleware/model/player.js
@@ -0,0 +1,17 @@
+const Schema = require('mongoose').Schema
+const db = require('../config/db')
+
+const Player = db.model('Player', {
+    name: String,
+    position: String,
+    ppg: Number,
+    trb: Number,
+    ast: Number,
+    mpg: Number,
+    _team: {
+        type: Schema.Types.ObjectId,
+        ref: 'Team'
+    }
+});
+
+module.exports = Player
